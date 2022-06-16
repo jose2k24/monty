9da6664b5ce3d89eb8a9466ff5587eb6516921c7
jose2k24 <josi.endal45@gmail.com>
2022-06-16 18:10:15 +0300

    C - Stacks, Queues - LIFO, FIFO

--- monty.h
+++ monty.h
@@ -1,11 +1,31 @@
-#ifndef MONTYH
-#define MONTYH
-
+#ifndef MONTY_H
+#define MONTY_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <fcntl.h>
 
-#define STACKMODE 0
-#define QUEUEMODE 1
+/**monty errors defined*/
+#define MONTY_ERROR_NONE 0
+#define MONTY_ERROR_INVALID_OPCODE 1
+#define MONTY_ERROR_PUSH_MISSING_ARG 2
+#define MONTY_ERROR_PUSH_INVALID_ARG 3
+#define MONTY_ERROR_PINT_EMPTY 4
+#define MONTY_ERROR_POP_EMPTY 5
+
+
+typedef struct monty_s{
+  char  *save_ptr;
+  int line;
+  char *token;
+  int mode;
+  int error;
+}monty_t;
+
+extern char* operand;
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -14,7 +34,7 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO project
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct stack_s
 {
@@ -24,12 +44,12 @@ typedef struct stack_s
 } stack_t;
 
 /**
- * struct instruction_s - opcoode and its function
+ * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO project
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct instruction_s
 {
@@ -37,47 +57,33 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-union montyfunctype
+/**
+ * struct line_s - line content and its number
+ * @content: line content
+ * @number: line number 
+ *
+ * Description: stores line of the monty source code
+ */
+typedef struct line_s
 {
-	void (*toponly)(stack_t **top);
-	void (*pushmode)(stack_t **top, stack_t **bot, int val, int mode);
-	void (*topbot)(stack_t **top, stack_t **bot);
-};
+	char *content;
+	int number;
+} line_t;
 
-typedef struct optype
-{
-	char *opcode;
-	union montyfunctype func;
-} optype;
+line_t *textfile_to_array(const char *filename);
+void op_push(stack_t **stack, unsigned int line_number);
+void op_pall(stack_t **stack, unsigned int line_number);
+void op_pint(stack_t **stack, unsigned int line_number);
+void op_pop(stack_t **stack, unsigned int line_number);
+void op_swap(stack_t **stack, unsigned int line_number);
 
-typedef struct montyglob
-{
-	char *buffer;
-	unsigned long linenum;
-	FILE* script;
-} montyglob;
+char **split_line(char *line);
+void (*get_op_func(char *s))(stack_t**, unsigned int);
 
-/* from montyparse.c */
-void exitwrap(int exitcode, char *existring, stack_t *top);
 
-/* opstack.c */
-void push(stack_t **top, stack_t **bot, int val, int mode);
-void pop(stack_t **top);
-void swap(stack_t **top, stack_t **bot);
-void rotl(stack_t **top, stack_t **bot);
-void rotr(stack_t **top, stack_t **bot);
-
-/* opprint.c */
-void pall(stack_t **top);
-void pint(stack_t **top);
-void pchar(stack_t **top);
-void pstr(stack_t **top);
-
-/* opmath.c */
-void add(stack_t **top);
-void sub(stack_t **top);
-void mul(stack_t **top);
-void _div(stack_t **top);
-void mod(stack_t **top);
+void free_lines(line_t *head);
+void free_stack(stack_t *head);
+int _atoi(char *s, int* n);
 
 #endif
+
