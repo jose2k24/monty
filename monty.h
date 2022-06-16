1e93083ab6ca151fa293c94560714d1ef94972ed
jose2k24 <josi.endal45@gmail.com>
2022-06-16 23:20:54 +0300

    C - Stacks, Queues - LIFO, FIFO

--- monty.h
+++ monty.h
@@ -1,17 +1,12 @@
 #ifndef MONTY_H
 #define MONTY_H
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <sys/types.h>
 #include <unistd.h>
-#include <stdbool.h>
-#include <ctype.h>
 #include <fcntl.h>
-
-#define STACKMODE 0
-#define QUEUEMODE 1
-
+#include <string.h>
+#include <ctype.h>
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -19,84 +14,65 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
-
+/**
+ * struct bus_s - variables -args, file, line content
+ * @arg: value
+ * @file: pointer to monty file
+ * @content: line content
+ * @lifi: flag change stack <-> queue
+ * Description: carries values through the program
+ */
+typedef struct bus_s
+{
+	char *arg;
+	FILE *file;
+	char *content;
+	int lifi;
+}  bus_t;
+extern bus_t bus;
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
-} instruction_t;
-
-union montyfunctype
-{
-	void (*toponly)(stack_t **top);
-	void (*pushmode)(stack_t **top, stack_t **bot, int val, int mode);
-	void (*topbot)(stack_t **top, stack_t **bot);
-};
-
-typedef struct optype
-{
 	char *opcode;
-	union montyfunctype func;
-} optype;
-
-typedef struct montyglob
-{
-	char *buffer;
-	unsigned long linenum;
-	FILE* script;
-} montyglob;
-
-/* from montyparse.c */
-void exitwrap(int exitcode, char *existring, stack_t *top);
-
-/* opstack.c */
-void push(stack_t **top, stack_t **bot, int val, int mode);
-void pop(stack_t **top);
-void swap(stack_t **top, stack_t **bot);
-void rotl(stack_t **top, stack_t **bot);
-void rotr(stack_t **top, stack_t **bot);
-
-/* opprint.c */
-void pall(stack_t **top);
-void pint(stack_t **top);
-void pchar(stack_t **top);
-void pstr(stack_t **top);
-
-/* opmath.c */
-void add(stack_t **top);
-void sub(stack_t **top);
-void mul(stack_t **top);
-void _div(stack_t **top);
-void mod(stack_t **top);
-
-
-/**monty errors defined*/
-
-typedef struct line_s
-{
-	char *content;
-	int number;
-} line_t;
-
-line_t *textfile_to_array(const char *filename);
-void free_lines(line_t *head);
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
+ssize_t getstdin(char **lineptr, int file);
+char  *clean_line(char *content);
+void f_push(stack_t **head, unsigned int number);
+void f_pall(stack_t **head, unsigned int number);
+void f_pint(stack_t **head, unsigned int number);
+int execute(char *content, stack_t **head, unsigned int counter, FILE *file);
 void free_stack(stack_t *head);
-int _atoi(char *s, int* n);
-
+void f_pop(stack_t **head, unsigned int counter);
+void f_swap(stack_t **head, unsigned int counter);
+void f_add(stack_t **head, unsigned int counter);
+void f_nop(stack_t **head, unsigned int counter);
+void f_sub(stack_t **head, unsigned int counter);
+void f_div(stack_t **head, unsigned int counter);
+void f_mul(stack_t **head, unsigned int counter);
+void f_mod(stack_t **head, unsigned int counter);
+void f_pchar(stack_t **head, unsigned int counter);
+void f_pstr(stack_t **head, unsigned int counter);
+void f_rotl(stack_t **head, unsigned int counter);
+void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter);
+void addnode(stack_t **head, int n);
+void addqueue(stack_t **head, int n);
+void f_queue(stack_t **head, unsigned int counter);
+void f_stack(stack_t **head, unsigned int counter);
 #endif
